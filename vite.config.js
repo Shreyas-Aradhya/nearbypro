30fbf6f75e00b20c0aa2074c9ebb0829d1602fd3
Shreyas Aradhya <shreyas.s@ticketsque.com>
2023-06-21 19:04:18 +0530

    Added render for hosting

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "http://shreyas-aradhya.github.io/nearbypro/",
+  base: "https://nearby-pro.onrender.com/",
   plugins: [react()],
 });
