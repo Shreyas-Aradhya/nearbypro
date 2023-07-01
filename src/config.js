ff12a229cdf46a8a2593a7da3bfcfa24794992ad
Shreyas Aradhya <shreyas.s@ticketsque.com>
2023-07-01 20:52:16 +0530

    Prep for production

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-// export const HOST_API =
-//   "http://ec2-43-204-229-119.ap-south-1.compute.amazonaws.com:8001/v1";
+export const HOST_API =
+  "http://ec2-43-204-229-119.ap-south-1.compute.amazonaws.com:8001/v1";
 
-export const HOST_API = "http://localhost:8001/v1";
+// export const HOST_API = "http://localhost:8001/v1";
