f0644efac01eb71853f7072a3b67654ec74b3e7c
Shreyas Aradhya <shreyas.s@ticketsque.com>
2023-07-26 12:14:01 +0530

    Update: using ip for api calls on server

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
-// export const HOST_API =
-//   "http://ec2-43-204-229-119.ap-south-1.compute.amazonaws.com:8000/v1";
+export const HOST_API = "http://43.205.135.94:8000/v1";
 
-export const HOST_API = "http://localhost:8000/v1";
+// export const HOST_API = "http://localhost:8000/v1";
