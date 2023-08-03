3e3bf1c878b9b6b44aae7861fa2c099844adbe7d
Shreyas Aradhya <shreyas.s@ticketsque.com>
2023-08-03 22:44:20 +0530

    Stagging ready

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-// export const HOST_API = "http://43.205.135.94:8000/v1";
+export const HOST_API = "http://43.205.135.94:8000/v1";
 
-export const HOST_API = "http://localhost:8000/v1";
+// export const HOST_API = "http://localhost:8000/v1";
