349c5d2a0229f14256ea18c528923edc48f4bb8c
Shreyas Aradhya <shreyas.s@ticketsque.com>
2023-09-20 11:17:06 +0530

    About content bug fix

--- src/sections/HomePage/TwoColumnAbout/TwoColumnAbout.jsx
+++ src/sections/HomePage/TwoColumnAbout/TwoColumnAbout.jsx
@@ -15,7 +15,7 @@ const ContentRow = ({ title, text, img, alt, listTitle, listItems }) => {
           <h3>{title}</h3>
         </div>
         <div className={styles["content-txt"]}>
-          {typeof text === "Array" ? (
+          {typeof text === "object" ? (
             text?.map((p, index) => <p key={index}>{p}</p>)
           ) : (
             <p>{text}</p>
